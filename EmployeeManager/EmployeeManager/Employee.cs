ff28344083dcaa0c44a02ac9aaa6b00dd67c2649
mm1009057 <mm1009057@otc.edu>
2025-01-30 10:12:10 -0600

    Initial Commit
    Start

---
+++ EmployeeManager/EmployeeManager/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManager
+{
+    internal class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public decimal BaseSalary { get; set; }
+
+        public Employee(int id, string name, string department, decimal BaseSalary)
+        {
+            ID = id;
+            Name = name;
+            Department = department;
+            BaseSalary = basesalary;
+        }
+    }
+}
